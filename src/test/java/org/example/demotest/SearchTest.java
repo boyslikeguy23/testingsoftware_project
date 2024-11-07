e60e0c33e9daa0be2344aa076af45d355cf60ecb
kuangbuckk <kuangbuckk159@gmail.com>
2024-11-07 16:08:53 +0700

    Add search existing event

--- src/test/java/org/example/demotest/SearchTest.java
+++ src/test/java/org/example/demotest/SearchTest.java
@@ -13,47 +13,63 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 public class SearchTest {
     private WebDriver webDriver;
+    String homePageURL = "https://ticketbox.vn/";
 
     @Before
     public void setUp() throws Exception {
         webDriver = new ChromeDriver();
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        webDriver.navigate().to("https://ticketbox.vn/");
+        webDriver.navigate().to(homePageURL);
     }
 
     @Test
     public void searchNothing() {
         By searchBtnLocation = RelativeLocator.with(By.tagName("button")).toRightOf(By.id("search-input"));
         WebElement btnSearch = webDriver.findElement(searchBtnLocation);
-        System.out.println(btnSearch);
         btnSearch.click();
 
         WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(5));
         wait.until(ExpectedConditions.urlToBe("https://ticketbox.vn/search?q="));
 
         String urlCurrent = webDriver.getCurrentUrl();
-        System.out.println(urlCurrent);
-        Assert.assertEquals("https://ticketbox.vn/search?q=", urlCurrent);
+        Assert.assertEquals(homePageURL, urlCurrent);
     }
 
     @Test
-    public void searchSomething() {
+    public void searchNonExistingEvent(){
         WebElement txtSearch = webDriver.findElement(By.id("search-input"));
-        txtSearch.sendKeys("Hà Nội");
+        txtSearch.sendKeys("XYZ123");
         By searchBtnLocation = RelativeLocator.with(By.tagName("button")).toRightOf(By.id("search-input"));
         WebElement btnSearch = webDriver.findElement(searchBtnLocation);
         btnSearch.click();
 
         WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(5));
-        wait.until(ExpectedConditions.urlContains("https://ticketbox.vn/search?q=Hà+Nội"));
+        wait.until(ExpectedConditions.urlToBe("https://ticketbox.vn/search?q=XYZ123"));
 
-        String urlCurrent = webDriver.getCurrentUrl();
-        System.out.println(urlCurrent);
-        Assert.assertEquals("https://ticketbox.vn/search?q=Hà+Nội", urlCurrent);
+        WebElement noResultMessage = webDriver.findElement(By.xpath("//div[contains(text(), 'Rất tiếc! Không tìm thấy kết quả nào')]"));
+        Assert.assertNotNull(noResultMessage);
+    }
+
+    @Test
+    public void searchExistingEventWithKeyword() {
+        WebElement txtSearch = webDriver.findElement(By.id("search-input"));
+        txtSearch.sendKeys("Hanoi");
+        By searchBtnLocation = RelativeLocator.with(By.tagName("button")).toRightOf(By.id("search-input"));
+        WebElement btnSearch = webDriver.findElement(searchBtnLocation);
+        btnSearch.click();
+
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.urlContains("https://ticketbox.vn/search?q=Hanoi"));
+
+        // Tìm các thẻ chứa title là "Hanoi"
+        List<WebElement> results = webDriver.findElements(By.xpath("//*[contains(text(), 'Hanoi')]"));
+
+        Assert.assertFalse("Ít nhất 1 thẻ sự kiện chứa chữ Hà Nội", results.isEmpty());
     }
 
     @After
