b8c5920e6e9a68f5825274869cc8280a2edac54b
kuangbuckk <kuangbuckk159@gmail.com>
2024-11-07 15:28:35 +0700

    Remove redundants

--- src/main/java/org/example/demotest/DemoTestApplication.java
+++ src/main/java/org/example/demotest/DemoTestApplication.java
@@ -1,13 +1,9 @@
 package org.example.demotest;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class DemoTestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoTestApplication.class, args);
+		System.out.println("Hello World!");
 	}
 
 }
