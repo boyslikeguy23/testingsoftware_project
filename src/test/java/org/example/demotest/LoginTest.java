525e223731400e45e999cdc3c71de2fca3fec9ee
boyslikeguy23 <tungxuanmai2003@gmail.com>
2024-11-07 16:39:42 +0700

    Hoan thanh phan Assert Login, thieu dang nhap thanh cong

--- src/test/java/org/example/demotest/LoginTest.java
+++ src/test/java/org/example/demotest/LoginTest.java
@@ -9,9 +9,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.locators.RelativeLocator;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 import java.time.Duration;
 
